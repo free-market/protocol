5635cabba76985050658684d502ea250c52d2cde
Finley Breese <finley@fmprotocol.com>
2022-05-13 18:45:24 -0400

    test: add steps for swapping USDT -> USDC

--- contracts-spec/fmp.feature
+++ contracts-spec/fmp.feature
@@ -1,9 +1,11 @@
 Feature: fmp
   Scenario: I can deposit
     Given I have an account with 1000 ETH
-    When I exchange 1 ETH for WETH
-    And I exchange 1 WETH for USDT
-    Then I should have some USDT
+    When I exchange some ETH for WETH
+    And I exchange some WETH for USDT
+    And I exchange some USDT for USDC
+    Then I should have some USDC
+    # And I deposit into the protocol
     # When I call tricrypto.exchange(2, 0, '100000000000000000', '10000000000000000', true)
     # And I call fmp.deposit(USDC_ADDRESS, '10000000000000000')
     #Then my deposit logs should match the snapshot
--- contracts-spec/fmp.spec.ts
+++ contracts-spec/fmp.spec.ts
@@ -1,11 +1,14 @@
 import * as ethers from 'ethers'
 import FreeMarket from '@fmp/contracts/build/contracts/FreeMarket.json'
 import TricryptoPool from '@fmp/contracts/TricryptoPool.json'
+import ThreePool from '@fmp/contracts/ThreePool.json'
 import WrappedEther from '@fmp/contracts/WrappedEther.json'
 import Parser from 'gherkin/dist/src/Parser'
 import AstBuilder from 'gherkin/dist/src/AstBuilder'
 import { IdGenerator } from '@cucumber/messages'
 import { readFileSync } from 'fs'
+
+jest.setTimeout(30 * 1000)
 
 const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545')
 
@@ -34,11 +37,21 @@ const tricrypto = new ethers.Contract(
   account
 )
 
+    const THREEPOOL_ADDRESS = '0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7'
+      const threePool = new ethers.Contract(
+        THREEPOOL_ADDRESS,
+        ThreePool as any,
+        account
+    )
+
 const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
 const weth = new ethers.Contract(WETH_ADDRESS, WrappedEther, account)
 
 const USDT_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
-const usdt = new ethers.Contract(USDT_ADDRESS, WrappedEther.filter(({name}) => name === 'balanceOf'), account)
+const usdt = new ethers.Contract(USDT_ADDRESS, WrappedEther, account)
+
+const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+const usdc = new ethers.Contract(USDC_ADDRESS, WrappedEther.filter(({name}) => name === 'balanceOf'), account)
 
 const astBuilder = new AstBuilder(IdGenerator.incrementing())
 
@@ -55,19 +68,15 @@ describe('FreeMarket contract', () => {
       await contract.deployed()
 
       for (const step of scenario.steps) {
-        if (step.text === 'I exchange 1 WETH for USDT') {
+        if (step.text === 'I exchange some WETH for USDT') {
+
           const wethBalance = await weth.balanceOf(account.address)
-            const [expected] = await tricrypto.functions.get_dy(2, 0, wethBalance, { gasLimit: 3000000000 })
-            console.log(expected.toString())
-            console.log(tricrypto)
+            const [expected] = await tricrypto.functions.get_dy(2, 0, wethBalance, { gasLimit: 100000} )
           const approvalTx = await weth['approve'](
             tricrypto.address,
             wethBalance,
           )
-          console.log('approvalTx', approvalTx)
           const approvalReceipt = await approvalTx.wait()
-          console.log('approvalReceipt', approvalReceipt)
-          console.log('approved')
           const tx = await tricrypto['exchange(uint256,uint256,uint256,uint256,bool)'](
             2, // weth
             0, // usdt
@@ -76,7 +85,24 @@ describe('FreeMarket contract', () => {
             false
           )
           await tx.wait()
-        } else if (step.text === 'I exchange 1 ETH for WETH') {
+
+        } else if (step.text === 'I exchange some USDT for USDC') {
+          const usdtBalance = await usdt.balanceOf(account.address)
+            const [expected] = await threePool.functions.get_dy(2, 1, usdtBalance, {gasLimit: 100000})
+            console.log('expected usdc', expected.toString())
+          const approvalTx = await usdt.approve(
+            threePool.address,
+            expected,
+          )
+          const approvalReceipt = await approvalTx.wait()
+          const tx = await threePool.exchange(
+            2, // usdt
+            1, // usdc
+            expected,
+            1
+          )
+          await tx.wait()
+        } else if (step.text === 'I exchange some ETH for WETH') {
           const tx = await weth.functions.deposit({ value: '100000000000000000' })
           await tx.wait()
           const wethBalance = (await weth.balanceOf(account.address)).toString()
@@ -84,6 +110,9 @@ describe('FreeMarket contract', () => {
           // expect(wethBalance).toBe('10000000000000000')
         } else if (step.text === 'I should have some USDT') {
           const balance = await usdt.balanceOf(account.address)
+          expect(balance.toString()).not.toBe('0')
+        } else if (step.text === 'I should have some USDC') {
+          const balance = await usdc.balanceOf(account.address)
           expect(balance.toString()).not.toBe('0')
         }
       }
