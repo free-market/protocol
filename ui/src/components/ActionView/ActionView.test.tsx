dbde9517ef36b1406dc469333c02f183dd65c074
0xGordonFreeman <gordon@fmprotocol.com>
2022-09-28 15:27:52 -0500

    added turborepo

--- ui/src/components/ActionView/ActionView.test.tsx
+++ ui/src/components/ActionView/ActionView.test.tsx
@@ -1,10 +1,13 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import { buildWorkflow } from 'utils'
 import ActionView from './'
 
 describe('Component: ActionView', () => {
   it('should render children', () => {
-    render(<ActionView>sentinel value</ActionView>)
+    const workflow = buildWorkflow()
+
+    render(<ActionView step={workflow.steps[0]}>sentinel value</ActionView>)
 
     expect(screen.getByText('sentinel value')).toBeInTheDocument()
   })
