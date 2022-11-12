da038e7e55b21c03af64e4db1a370bc868c86cb7
Finley Breese <finley@fmprotocol.com>
2022-11-12 18:25:51 -0500

    feat(ui2): build out initial design for step choices

--- ui2/src/components/StepBuilder/StepBuilder.tsx
+++ ui2/src/components/StepBuilder/StepBuilder.tsx
@@ -1,4 +1,5 @@
 import {ChevronLeftIcon} from '@heroicons/react/20/solid'
+import {PlusIcon} from '@heroicons/react/24/solid'
 import { motion } from 'framer-motion'
 import { useCore } from '../CoreProvider'
 
@@ -35,14 +36,34 @@ export const StepBuilder = (): JSX.Element => {
             <ChevronLeftIcon className="w-4 h-4 mx-2" />
             <div>Curve</div>
           </motion.div>
-          <motion.div variants={variants} initial="hidden" animate="visible" exit="hidden" key={core.selectedActionGroupName} className="flex justify-center flex-col" transition={{ transition: { delay: 0.2 } }}>
-          <div className="bg-zinc-700 p-2 rounded-xl cursor-pointer hover:bg-zinc-600 active:bg-zinc-600/75 select-none flex items-center justify-between group">
-            <div className="flex items-center">
-              <img src='https://curve.fi/favicon-32x32.png' className="w-8 h-8"/>
-              <div className="text-zinc-300 text-lg px-2">Curve</div>
+          <motion.div variants={variants} initial="hidden" animate="visible" exit="hidden" key={core.selectedActionGroupName} className="flex items-center flex-col content-end" transition={{ transition: { delay: 0.2 } }}>
+            <div className="inline-flex bg-zinc-700 py-2 px-4 rounded-xl cursor-pointer hover:bg-zinc-600 active:bg-zinc-600/75 select-none items-center justify-between group flex-col space-y-5">
+              <div className="inline-flex items-center w-full justify-between">
+              <div className="inline-flex items-center">
+                <img src='https://curve.fi/favicon-32x32.png' className="w-5 h-5 group-active:opacity-75"/>
+                <div className="text-zinc-300 px-2">Swap</div>
+              </div>
+                <PlusIcon className="w-8 h-8 text-zinc-600 group-hover:text-zinc-400/50 group-active:text-zinc-500"/>
+              </div>
+              <div className="flex items-center text-zinc-600 group-hover:text-zinc-400/50 group-active:text-zinc-500">
+                <div className="flex items-center rounded-full bg-zinc-500 text-zinc-300/75 py-1 px-2 space-x-2">
+                  <div className="rounded-full overflow-hidden w-5 h-5">
+                    <img src="https://res.cloudinary.com/sushi-cdn/image/fetch/w_48,f_auto,q_auto,fl_sanitize/https://raw.githubusercontent.com/sushiswap/icons/master/token/usdc.jpg"/>
+                  </div>
+                  <span>USDC</span>
+                </div>
+
+                &nbsp;&nbsp;&nbsp;&rarr;&nbsp;&nbsp;&nbsp;{' '}
+
+                <div className="flex items-center rounded-full bg-zinc-500 text-zinc-300/75 py-1 px-2 space-x-2">
+                  <div className="rounded-full overflow-hidden w-5 h-5">
+                    <img src="https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0xdac17f958d2ee523a2206206994597c13d831ec7.png"/>
+                  </div>
+                  <span>USDT</span>
+                </div>
+              </div>
             </div>
-          </div>
-        </motion.div>
+          </motion.div>
         </>
     )
   }
