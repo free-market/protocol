1a07017a02aaeefbea80d07173737ef506f94d45
Finley Breese <finley@fmprotocol.com>
2022-11-13 22:17:16 -0500

    feat(ui2, src/App.tsx): integrate app

--- ui2/src/App.tsx
+++ ui2/src/App.tsx
@@ -3,9 +3,15 @@ import './material-dark.css'
 import './solarized-dark-atom.css'
 import './fonts.css'
 import './magic-box.css'
+import Layout from '@component/Layout'
+import CoreProvider from '@component/CoreProvider'
 
 function App(): JSX.Element {
-  return <div>Hello world</div>
+  return (
+    <CoreProvider>
+      <Layout />
+    </CoreProvider>
+  )
 }
 
 export default App
