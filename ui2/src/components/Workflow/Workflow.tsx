2e9cd67b7ac77ed58ca415a6dc858d2cf345c78d
Finley Breese <finley@fmprotocol.com>
2022-11-13 20:55:26 -0500

    feat(ui2): update Workflow with an example step

--- ui2/src/components/Workflow/Workflow.tsx
+++ ui2/src/components/Workflow/Workflow.tsx
@@ -1,7 +1,57 @@
+import AssetPill from '@component/AssetPill'
+import { useCore } from '@component/CoreProvider'
+
 export const Workflow = (): JSX.Element => {
+  const core = useCore()
+
+  const handleMouseEnter = () => {
+    core.startPreviewingWorkflowStep('#1')
+  }
+
+  const handleMouseLeave = () => {
+    core.stopPreviewingWorkflowStep()
+  }
+
+  const inputPill = (
+    <AssetPill
+      className="inline-flex items-center rounded-full bg-zinc-700/75 text-zinc-300 py-1 px-2 space-x-2 font-medium text-lg group-hover:bg-zinc-600/75"
+      asset={{
+        label: 'USDC',
+        icon: {
+          url: 'https://res.cloudinary.com/sushi-cdn/image/fetch/w_48,f_auto,q_auto,fl_sanitize/https://raw.githubusercontent.com/sushiswap/icons/master/token/usdc.jpg',
+        },
+      }}
+    />
+  )
+
+  const outputPill = (
+    <AssetPill
+      className="inline-flex items-center rounded-full bg-zinc-700/75 text-zinc-300 py-1 px-2 space-x-2 font-medium text-lg group-hover:bg-zinc-600/75"
+      asset={{
+        label: 'USDT',
+        icon: { url: 'https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0xdac17f958d2ee523a2206206994597c13d831ec7.png' },
+      }}
+    />
+  )
   return (
     <>
-      <p className="text-zinc-200">Hello World</p>
+      <div
+        className="rounded-xl bg-zinc-700/25 w-full p-2 hover:bg-zinc-700/50 cursor-pointer group"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className="inline-flex items-center gap-2 justify-between w-full">
+          <div className="inline-flex items-center gap-2">
+            <div className="text-zinc-400">#1</div>
+            <img src="https://curve.fi/favicon-32x32.png" className="w-5 h-5" />
+            <div className="text-zinc-400">Curve Swap</div>
+          </div>
+          <div className="flex items-center text-zinc-600 group-hover:text-zinc-500/50">
+            {inputPill}
+            &nbsp;&nbsp;&nbsp;&rarr;&nbsp;&nbsp;&nbsp; {outputPill}
+          </div>
+        </div>
+      </div>
     </>
   )
 }
