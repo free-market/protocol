9087d92392543a4e05f7652bfc2983de22e206a9
Finley Breese <finley@fmprotocol.com>
2022-11-17 11:19:55 -0500

    feat(ui2, config): parameterize input and output

--- ui2/src/components/StepChoiceCard/StepChoiceCard.tsx
+++ ui2/src/components/StepChoiceCard/StepChoiceCard.tsx
@@ -9,33 +9,17 @@ export const StepChoiceCard = (props: { index?: number }): JSX.Element => {
   const { index = 0 } = props
   const core = useCore()
 
+  if (core.selectedActionGroupName == null) {
+    throw new Error('StepChoiceCard: selectedActionGroupName required')
+  }
+
   const click = () => {
     core.selectStepChoice('swap')
   }
 
-  const inputPill = (
-    <AssetPill
-      asset={{
-        label: 'USDC',
-        icon: {
-          url: 'https://res.cloudinary.com/sushi-cdn/image/fetch/w_48,f_auto,q_auto,fl_sanitize/https://raw.githubusercontent.com/sushiswap/icons/master/token/usdc.jpg',
-        },
-      }}
-    />
-  )
+  const inputPill = <AssetPill asset={catalog[core.selectedActionGroupName].actions[0].input.asset} />
 
-  const outputPill = (
-    <AssetPill
-      asset={{
-        label: 'USDT',
-        icon: { url: 'https://cdn.jsdelivr.net/gh/curvefi/curve-assets/images/assets/0xdac17f958d2ee523a2206206994597c13d831ec7.png' },
-      }}
-    />
-  )
-
-  if (core.selectedActionGroupName == null) {
-    throw new Error('StepChoiceCard: selectedActionGroupName required')
-  }
+  const outputPill = <AssetPill asset={catalog[core.selectedActionGroupName].actions[0].output.asset} />
 
   return (
     <motion.button
--- ui2/src/config.ts
+++ ui2/src/config.ts
@@ -1,7 +1,14 @@
 import { ActionGroupName } from '@component/CoreProvider/CoreProvider'
 
+export type CatalogAsset = {
+  label: string
+  icon: { url: string }
+}
+
 export type CatalogAction = {
   title: string
+  input: { asset: CatalogAsset }
+  output: { asset: CatalogAsset }
 }
 
 export type CatalogGroup = {
@@ -18,20 +25,80 @@ export const catalog: Catalog = {
     name: 'curve',
     title: 'Curve',
     icon: { url: 'https://curve.fi/favicon-32x32.png' },
-    actions: [{ title: 'Swap' }],
+    actions: [
+      {
+        title: 'Swap',
+        input: {
+          asset: {
+            label: '1INCH',
+            icon: {
+              url: 'https://app.aave.com/icons/tokens/1inch.svg',
+            },
+          },
+        },
+        output: {
+          asset: {
+            label: 'USDC',
+            icon: {
+              url: 'https://res.cloudinary.com/sushi-cdn/image/fetch/w_48,f_auto,q_auto,fl_sanitize/https://raw.githubusercontent.com/sushiswap/icons/master/token/usdc.jpg',
+            },
+          },
+        },
+      },
+    ],
   },
 
   zksync: {
     name: 'zksync',
     title: 'zkSync',
     icon: { url: '/zksync.svg' },
-    actions: [{ title: 'Bridge' }],
+    actions: [
+      {
+        title: 'Bridge',
+        input: {
+          asset: {
+            label: 'USDC',
+            icon: {
+              url: 'https://res.cloudinary.com/sushi-cdn/image/fetch/w_48,f_auto,q_auto,fl_sanitize/https://raw.githubusercontent.com/sushiswap/icons/master/token/usdc.jpg',
+            },
+          },
+        },
+        output: {
+          asset: {
+            label: 'USDC',
+            icon: {
+              url: 'https://res.cloudinary.com/sushi-cdn/image/fetch/w_48,f_auto,q_auto,fl_sanitize/https://raw.githubusercontent.com/sushiswap/icons/master/token/usdc.jpg',
+            },
+          },
+        },
+      },
+    ],
   },
 
   aave: {
     name: 'aave',
     title: 'Aave',
     icon: { url: 'https://app.aave.com/icons/tokens/aave.svg' },
-    actions: [{ title: 'Borrow' }],
+    actions: [
+      {
+        title: 'Borrow',
+        input: {
+          asset: {
+            label: 'USDC',
+            icon: {
+              url: 'https://res.cloudinary.com/sushi-cdn/image/fetch/w_48,f_auto,q_auto,fl_sanitize/https://raw.githubusercontent.com/sushiswap/icons/master/token/usdc.jpg',
+            },
+          },
+        },
+        output: {
+          asset: {
+            label: 'USDC',
+            icon: {
+              url: 'https://res.cloudinary.com/sushi-cdn/image/fetch/w_48,f_auto,q_auto,fl_sanitize/https://raw.githubusercontent.com/sushiswap/icons/master/token/usdc.jpg',
+            },
+          },
+        },
+      },
+    ],
   },
 }
