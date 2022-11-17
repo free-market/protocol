8a17bfd5223fdcdf5e0d8628fd2e3a275040c5b3
Finley Breese <finley@fmprotocol.com>
2022-11-17 11:03:34 -0500

    feat(ui2): parameterize step choice card titles

--- ui2/src/config.ts
+++ ui2/src/config.ts
@@ -1,7 +1,37 @@
 import { ActionGroupName } from '@component/CoreProvider/CoreProvider'
 
-export const headingMap: Record<ActionGroupName, { title: string; icon: { url: string } }> = {
-  curve: { title: 'Curve', icon: { url: 'https://curve.fi/favicon-32x32.png' } },
-  zksync: { title: 'zkSync', icon: { url: '/zksync.svg' } },
-  aave: { title: 'Aave', icon: { url: 'https://app.aave.com/icons/tokens/aave.svg' } },
+export type CatalogAction = {
+  title: string
+}
+
+export type CatalogGroup = {
+  name: ActionGroupName
+  title: string
+  icon: { url: string }
+  actions: CatalogAction[]
+}
+
+export type Catalog = Record<ActionGroupName, CatalogGroup>
+
+export const catalog: Catalog = {
+  curve: {
+    name: 'curve',
+    title: 'Curve',
+    icon: { url: 'https://curve.fi/favicon-32x32.png' },
+    actions: [{ title: 'Swap' }],
+  },
+
+  zksync: {
+    name: 'zksync',
+    title: 'zkSync',
+    icon: { url: '/zksync.svg' },
+    actions: [{ title: 'Bridge' }],
+  },
+
+  aave: {
+    name: 'aave',
+    title: 'Aave',
+    icon: { url: 'https://app.aave.com/icons/tokens/aave.svg' },
+    actions: [{ title: 'Borrow' }],
+  },
 }
