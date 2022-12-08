dc2bcd774a3fdd9fa45623c1074452372338a451
Finley Breese <finley@fmprotocol.com>
2022-12-08 10:42:48 -0500

    feat(FMP-294): input element for form

--- ui2/src/components/CrossChainDepositLayout/CrossChainDepositLayout.tsx
+++ ui2/src/components/CrossChainDepositLayout/CrossChainDepositLayout.tsx
@@ -10,32 +10,50 @@ export const CrossChainDepositLayout = (): JSX.Element => {
       </div>
 
       <div className="bg-zinc-700 rounded-xl p-2 space-y-2 max-w-sm mx-auto shadow-md">
+        {/* TODO(FMP-293): support hover, active states */}
         <button className="w-full text-left bg-zinc-600 p-2 rounded-xl space-y-2">
           <div className="text-xs text-zinc-400 font-light">CHAIN</div>
           <div className="flex items-center gap-2">
-            <div className="rounded-full overflow-hidden w-5 h-5 bg-zinc-500">
-              <img className="w-full h-full" src={url} />
+            <div className="rounded-full overflow-hidden w-4 h-4 bg-zinc-500">
+              <img className="w-full h-full opacity-[0.9]" src={url} />
             </div>
             <div className="text-zinc-300">Ethereum</div>
           </div>
         </button>
 
+        {/* TODO(FMP-293): support hover, active states */}
         <button className="w-full text-left bg-zinc-600 p-2 rounded-xl space-y-2">
           <div className="text-xs text-zinc-400 font-light">TOKEN</div>
           <div className="flex items-center gap-2">
-            <div className="rounded-full overflow-hidden w-5 h-5 bg-zinc-500">
-              <img className="w-full h-full" src={url} />
+            <div className="rounded-full overflow-hidden w-4 h-4 bg-zinc-500">
+              <img className="w-full h-full opacity-[0.9]" src={url} />
             </div>
             <div className="text-zinc-300 font-medium">ETH</div>
           </div>
         </button>
 
-        <div className="bg-zinc-600 p-2 rounded-xl space-y-2">
-          <div className="text-xs text-zinc-400 font-light">AMOUNT</div>
-          <div className="flex items-center gap-2">
-            <div className="text-zinc-300 font-medium">0.00</div>
-          </div>
+        <div className="bg-zinc-600 pt-2 rounded-xl">
+          <div className="text-xs text-zinc-400 font-light ml-2">AMOUNT</div>
+          <div className="flex items-center gap-2"></div>
+          <input
+            inputMode="decimal"
+            step="0.00000001"
+            title="Token Amount"
+            autoComplete="off"
+            autoCorrect="off"
+            type="text"
+            pattern="^\d*(\.\d{0,2})?$"
+            placeholder="0.00"
+            min="0"
+            minLength={1}
+            maxLength={79}
+            spellCheck={false}
+            autoFocus
+            className="relative font-bold outline-none border-none flex-auto overflow-hidden overflow-ellipsis placeholder-low-emphesis focus:placeholder-primary focus:placeholder:text-low-emphesis focus:outline-2 flex-grow text-left bg-transparent placeholder:text-zinc-400 text-zinc-200 rounded-2xl px-2 pb-3 pt-8 -mt-6 hover:bg-zinc-500/25 disabled:opacity-50 disabled:cursor-not-allowed disabled:!bg-transparent w-full"
+          />
         </div>
+
+        {/* TODO(FMP-293): support hover, active states */}
         <button className="w-full bg-zinc-600 p-2 text-zinc-200 rounded-xl text-center bg-sky-600 font-medium text-xl">Start</button>
       </div>
     </>
