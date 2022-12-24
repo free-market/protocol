a4c01bc7c04a832fc686d07c0514f56802fc0b46
Finley Breese <finley@fmprotocol.com>
2022-12-24 09:24:25 -0500

    refactor(CrossChainJobCard): improve digestibility

--- ui2/src/components/CrossChainJobCard/CrossChainJobCard.tsx
+++ ui2/src/components/CrossChainJobCard/CrossChainJobCard.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion'
 
 export const CrossChainJobCard = (props: {
   status?: 'completed' | 'sending'
-  transactionView?: 'tall' | 'compact' | 'compact-cols'
+  transactionView?: 'tall' | 'compact'
   spinnerLocation?: 'status' | 'transaction'
   pulseBehavior?: 'gradient' | 'pulse'
 }): JSX.Element => {
@@ -15,14 +15,134 @@ export const CrossChainJobCard = (props: {
     pulseBehavior = 'pulse',
   } = props
 
+  const transactionSpinner = (
+    <div className="inline-flex items-center relative z-30">
+      <svg
+        className="inline mx-1 w-3 h-3 text-transparent animate-spin fill-stone-600"
+        viewBox="0 0 100 101"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+          fill="currentColor"
+        />
+        <path
+          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+          fill="currentFill"
+        />
+      </svg>
+      <span className="sr-only">Loading...</span>
+    </div>
+  )
+  const transactionGradient = (
+    <motion.div
+      className="absolute -inset-4 rounded z-20 opacity-75"
+      initial={{ '--rotate': '0deg' } as Record<string, unknown>}
+      animate={{
+        ['--rotate' as string]: '359deg',
+        transition: {
+          repeat: Infinity,
+          repeatType: 'loop',
+          duration: 3,
+        },
+      }}
+      style={{
+        backgroundImage:
+          'linear-gradient(var(--rotate), #5ddcff99, #3c67e399 43%, #4e00c299)',
+        backgroundPosition: '0 -10px',
+      }}
+    />
+  )
+
+  const transactionPulse = (
+    <motion.div
+      initial={{
+        opacity: 0.5,
+      }}
+      animate={{
+        opacity: 1,
+        transition: {
+          repeat: Infinity,
+          repeatType: 'reverse',
+          duration: 1,
+        },
+      }}
+      className="absolute -inset-4 rounded z-20 bg-stone-200/50"
+    />
+  )
+
+  const avalanchePill = (
+    <div className="rounded bg-[#E84142] w-[calc(100%-2rem)] h-4 flex justify-between relative overflow-hidden border border-stone-600 hover:border-stone-300">
+      {spinnerLocation === 'transaction' && transactionSpinner}
+
+      {spinnerLocation === 'status' && <div></div>}
+
+      <div className="">
+        <div className="h-4 mx-4 overflow-hidden flex items-center">
+          <div className="w-6 h-6 relative">
+            <motion.img
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: { delay: 1.3 },
+              }}
+              className="w-full h-full"
+              src="https://app.aave.com/icons/networks/avalanche.svg"
+            />
+          </div>
+        </div>
+      </div>
+
+      {pulseBehavior === 'gradient' && transactionGradient}
+
+      {pulseBehavior === 'pulse' && transactionPulse}
+    </div>
+  )
+
+  const ethereumPill = (
+    <div className="rounded bg-[#627eea] w-[calc(100%-2rem)] h-4 flex justify-between relative overflow-hidden border border-stone-600 hover:border-stone-300">
+      <div></div>
+
+      <div className="">
+        <div className="h-4 mx-4 overflow-hidden flex items-center">
+          <div className="w-6 h-6 relative">
+            <motion.img
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: { delay: 1.3 },
+              }}
+              className="w-full h-full"
+              src="https://app.aave.com/icons/networks/ethereum.svg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
+  const compactTransactionView = (
+    <>
+      <div className={cx('flex', 'flex-col gap-1')}>
+        {ethereumPill}
+        {avalanchePill}
+      </div>
+    </>
+  )
+
   return (
     <>
-      <div className="bg-stone-600 rounded-xl shadow-md text-stone-200 font-medium p-2 max-w-xs mx-auto space-y-2 overflow-hidden">
-        <div className="text-lg leading-none">Cross-chain bridge</div>
+      <div className="w-full bg-stone-600 rounded-xl shadow-md text-stone-200 font-medium p-2 max-w-xs mx-auto space-y-2 overflow-hidden">
+        <div className="flex justify-between">
+          <div className="text-lg leading-none">Cross-chain bridge</div>
+        </div>
 
         <div className="flex items-stretch gap-2">
           <div className="basis-1/2 space-y-2 flex flex-col">
-            <div className="text-xs font-light leading-none">STATUS</div>
+            <div className="text-xs font-light leading-none text-left">
+              STATUS
+            </div>
 
             <div className="grow flex items-end h-3 leading-none">
               {status === 'completed' ? (
@@ -30,11 +150,11 @@ export const CrossChainJobCard = (props: {
               ) : (
                 <>
                   {spinnerLocation === 'status' && (
-                    <div className="inline-flex items-center">
+                    <div role="status">
                       <svg
-                        className="inline m-1 mr-2 w-3 h-3 text-transparent animate-spin fill-stone-200"
+                        aria-hidden="true"
+                        className="mr-2 w-8 h-8 text-stone-600 animate-spin fill-stone-200"
                         viewBox="0 0 100 101"
-                        fill="none"
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path
@@ -56,7 +176,9 @@ export const CrossChainJobCard = (props: {
           </div>
 
           <div className="basis-1/2 space-y-2">
-            <div className="text-xs font-light leading-none">TRANSACTION</div>
+            <div className="text-xs font-light leading-none text-left">
+              TRANSACTION
+            </div>
             <div className="flex flex-col justify-end gap-1">
               {transactionView === 'tall' && (
                 <div className="flex items-stretch gap-2">
@@ -109,120 +231,7 @@ export const CrossChainJobCard = (props: {
                 </div>
               )}
 
-              {(transactionView === 'compact' ||
-                transactionView === 'compact-cols') && (
-                <>
-                  <div
-                    className={cx(
-                      'flex',
-                      transactionView === 'compact-cols'
-                        ? 'gap-2'
-                        : 'flex-col gap-1',
-                    )}
-                  >
-                    <div
-                      className="rounded bg-[#E84142] w-[calc(100%-2rem)] h-4 flex justify-between relative overflow-hidden border border-stone-600 hover:border-stone-300"
-                      style={{}}
-                    >
-                      {spinnerLocation === 'transaction' && (
-                        <div className="inline-flex items-center relative z-30">
-                          <svg
-                            className="inline mx-1 w-3 h-3 text-transparent animate-spin fill-stone-600"
-                            viewBox="0 0 100 101"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                              fill="currentColor"
-                            />
-                            <path
-                              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                              fill="currentFill"
-                            />
-                          </svg>
-                          <span className="sr-only">Loading...</span>
-                        </div>
-                      )}
-
-                      <div className="">
-                        <div className="h-4 mx-4 overflow-hidden">
-                          <div className="w-8 h-8 relative -translate-y-2">
-                            <motion.img
-                              initial={{ opacity: 0 }}
-                              animate={{
-                                opacity: 1,
-                                transition: { delay: 1.3 },
-                              }}
-                              className="w-full h-full"
-                              src="https://app.aave.com/icons/networks/avalanche.svg"
-                            />
-                          </div>
-                        </div>
-                      </div>
-
-                      {pulseBehavior === 'gradient' && (
-                        <motion.div
-                          className="absolute -inset-4 rounded z-20 opacity-75"
-                          initial={
-                            { '--rotate': '0deg' } as Record<string, unknown>
-                          }
-                          animate={{
-                            ['--rotate' as string]: '359deg',
-                            transition: {
-                              repeat: Infinity,
-                              repeatType: 'loop',
-                              duration: 3,
-                            },
-                          }}
-                          style={{
-                            backgroundImage:
-                              'linear-gradient(var(--rotate), #5ddcff99, #3c67e399 43%, #4e00c299)',
-                            backgroundPosition: '0 -10px',
-                          }}
-                        ></motion.div>
-                      )}
-
-                      {pulseBehavior === 'pulse' && (
-                        <motion.div
-                          initial={{
-                            opacity: 0.1,
-                          }}
-                          animate={{
-                            opacity: 1,
-                            transition: {
-                              repeat: Infinity,
-                              repeatType: 'reverse',
-                              duration: 1,
-                            },
-                          }}
-                          className="absolute -inset-4 rounded z-20 bg-stone-200/50"
-                        ></motion.div>
-                      )}
-                    </div>
-
-                    <div className="rounded bg-[#627eea] w-[calc(100%-2rem)] h-4 flex justify-between relative overflow-hidden border border-stone-600 hover:border-stone-300">
-                      <div />
-
-                      <div className="">
-                        <div className="h-4 mx-4 overflow-hidden">
-                          <div className="w-8 h-8 relative -translate-y-2">
-                            <motion.img
-                              initial={{ opacity: 0 }}
-                              animate={{
-                                opacity: 1,
-                                transition: { delay: 1.3 },
-                              }}
-                              className="w-full h-full"
-                              src="https://app.aave.com/icons/networks/ethereum.svg"
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </>
-              )}
+              {transactionView === 'compact' && compactTransactionView}
             </div>
           </div>
         </div>
