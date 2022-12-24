1d3078efdf7666e054e0d8e2baf3f41bf2a572be
Finley Breese <finley@fmprotocol.com>
2022-12-24 14:48:43 -0500

    feat(FMP-336): initial proof-of-concept for miniview

--- ui2/src/components/CrossChainJobCard/CrossChainJobCard.tsx
+++ ui2/src/components/CrossChainJobCard/CrossChainJobCard.tsx
@@ -1,6 +1,7 @@
 import { ArrowRightIcon } from '@heroicons/react/20/solid'
 import cx from 'classnames'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useCallback, useState } from 'react'
 
 export const CrossChainJobCard = (props: {
   status?: 'completed' | 'sending'
@@ -15,6 +16,12 @@ export const CrossChainJobCard = (props: {
     pulseBehavior = 'pulse',
   } = props
 
+  const [clicked, setClicked] = useState(false)
+
+  const handleClick = useCallback(() => {
+    setClicked(!clicked)
+  }, [clicked])
+
   const transactionSpinner = (
     <div className="inline-flex items-center relative z-30">
       <svg
@@ -27,14 +34,17 @@ export const CrossChainJobCard = (props: {
           d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
           fill="currentColor"
         />
+
         <path
           d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
           fill="currentFill"
         />
       </svg>
+
       <span className="sr-only">Loading...</span>
     </div>
   )
+
   const transactionGradient = (
     <motion.div
       className="absolute -inset-4 rounded z-20 opacity-75"
@@ -73,7 +83,7 @@ export const CrossChainJobCard = (props: {
   )
 
   const avalanchePill = (
-    <div className="rounded bg-[#E84142] w-[calc(100%-2rem)] h-4 flex justify-between relative overflow-hidden border border-stone-600 hover:border-stone-300">
+    <div className="rounded bg-[#E84142] w-[calc(100%-2rem)] h-4 flex justify-between relative overflow-hidden border border-stone-600">
       {spinnerLocation === 'transaction' && transactionSpinner}
 
       {spinnerLocation === 'status' && <div></div>}
@@ -101,7 +111,7 @@ export const CrossChainJobCard = (props: {
   )
 
   const ethereumPill = (
-    <div className="rounded bg-[#627eea] w-[calc(100%-2rem)] h-4 flex justify-between relative overflow-hidden border border-stone-600 hover:border-stone-300">
+    <div className="rounded bg-[#627eea] w-[calc(100%-2rem)] h-4 flex justify-between relative overflow-hidden border border-stone-600">
       <div></div>
 
       <div className="">
@@ -122,20 +132,180 @@ export const CrossChainJobCard = (props: {
     </div>
   )
 
+  const currentTransactionIndicator = (
+    <div
+      className={cx('rounded border-2 border-stone-300 h-3', {
+        'animate-pulse': pulseBehavior !== 'pulse',
+      })}
+    ></div>
+  )
+
   const compactTransactionView = (
     <>
       <div className={cx('flex', 'flex-col gap-1')}>
-        {ethereumPill}
-        {avalanchePill}
+        <div className="relative">
+          <div className="absolute left-0 top-0 bottom-0 flex items-center">
+            {currentTransactionIndicator}
+          </div>
+
+          <div className="pl-2">{avalanchePill}</div>
+        </div>
+        <div className="relative">
+          <div className="absolute left-0 top-0 bottom-0 flex items-center"></div>
+
+          <div className="pl-2">{ethereumPill}</div>
+        </div>
       </div>
     </>
   )
 
   return (
     <>
-      <div className="w-full bg-stone-600 rounded-xl shadow-md text-stone-200 font-medium p-2 max-w-xs mx-auto space-y-2 overflow-hidden">
+      <div
+        onClick={handleClick}
+        className="w-full bg-stone-600 rounded-xl shadow-md text-stone-200 font-medium p-2 max-w-xs mx-auto space-y-2 overflow-hidden cursor-pointer group relative"
+      >
+        <div className="absolute inset-0 pointer-events-none group-hover:bg-stone-900/10 group-active:bg-stone-900/25 z-50"></div>
+
         <div className="flex justify-between">
           <div className="text-lg leading-none">Cross-chain bridge</div>
+        </div>
+
+        <div className="!m-0">
+          <AnimatePresence>
+            {clicked && (
+              <motion.div
+                className="h-[11.5rem]"
+                initial={{ opacity: 0, marginBottom: '-11.5rem', y: 10 }}
+                animate={{
+                  y: 0,
+                  opacity: 1,
+                  marginBottom: 0,
+                  transition: {
+                    default: { ease: 'anticipate', duration: 0.5 },
+                    /*
+                    default: {
+                      type: 'spring',
+                      duration: 0.1,
+                      stiffness: 200,
+                      mass: 1,
+                      damping: 10,
+                    },
+                     */
+                    opacity: {
+                      type: 'spring',
+                      duration: 0.5,
+                      delay: 0.5,
+                    },
+                    y: {
+                      type: 'spring',
+                      duration: 0.5,
+                      delay: 0.5,
+                    },
+                  },
+                }}
+                exit={{
+                  opacity: 0,
+                  marginBottom: '-11.5rem',
+                  y: 0,
+                  transition: {
+                    default: {
+                      type: 'spring',
+                      duration: 0.1,
+                      mass: 1,
+                      damping: 20,
+                    },
+                    opacity: {
+                      type: 'spring',
+                      duration: 0.5,
+                    },
+                    y: {
+                      type: 'spring',
+                      duration: 0.5,
+                    },
+                  },
+                }}
+              >
+                <div className="space-y-2 pt-2">
+                  <div className="mx-4 saturate-[0.9] rounded-full bg-[#E84142] h-9 flex justify-between items-center relative overflow-hidden p-2">
+                    <div className="grow flex items-center rounded-full bg-stone-600 px-2">
+                      <div className="font-mono font-bold text-stone-500 flex items-center">
+                        <span className="text-xs">01 /</span>{' '}
+                        <span className="text-stone-400 font-[Inter] font-bold pl-2">
+                          Avalanche
+                        </span>
+                      </div>
+                    </div>
+                    <div className="h-8 mx-4 overflow-hidden flex items-center justify-end">
+                      <div className="w-8 h-8 relative">
+                        <motion.img
+                          initial={{ opacity: 0 }}
+                          animate={{
+                            opacity: 1,
+                            transition: { delay: 1.3 },
+                          }}
+                          className="w-full h-full"
+                          src="https://app.aave.com/icons/networks/avalanche.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mx-4 saturate-[0.9] rounded-full bg-[#627eea] h-9 flex justify-between items-center relative overflow-hidden border border-stone-600 p-2">
+                    <div className="grow flex items-center rounded-full bg-stone-600 px-2">
+                      <div className="font-mono font-bold text-stone-500 flex items-center">
+                        <span className="text-xs">02 /</span>{' '}
+                        <span className="text-stone-400 font-[Inter] font-bold pl-2">
+                          Ethereum
+                        </span>
+                      </div>
+                    </div>
+                    <div className="h-8 mx-4 overflow-hidden flex items-center justify-end">
+                      <div className="w-8 h-8 relative">
+                        <motion.img
+                          initial={{ opacity: 0 }}
+                          animate={{
+                            opacity: 1,
+                            transition: { delay: 1.3 },
+                          }}
+                          className="w-full h-full"
+                          src="https://app.aave.com/icons/networks/ethereum.svg"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="space-y-1">
+                    <div className="flex items-center justify-between text-xs px-4 border border-transparent text-stone-300 font-light">
+                      <div>Slippage</div>
+                      <div>0.5%</div>
+                    </div>
+                    <div className="flex items-center justify-between text-xs px-4 border border-transparent text-stone-300 font-light">
+                      <div>Gas on destination</div>
+                      <div>0.00235148 ETH</div>
+                    </div>
+                    <div className="flex items-center justify-between text-xs px-4 border border-transparent text-stone-400 font-light">
+                      <div>Fee</div>
+                      <div className="flex items-center gap-1">
+                        $3.00{' '}
+                        <svg
+                          className="w-4 h-4 fill-stone-400"
+                          focusable="false"
+                          viewBox="0 0 24 24"
+                          aria-hidden="true"
+                        >
+                          <path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="flex items-center justify-between text-xs px-4 border border-transparent text-stone-400 font-light">
+                      <div>Gas cost</div>
+                      <div>1.435278 AVAX</div>
+                    </div>
+                  </div>
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
 
         <div className="flex items-stretch gap-2">
@@ -161,14 +331,17 @@ export const CrossChainJobCard = (props: {
                           d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
                           fill="currentColor"
                         />
+
                         <path
                           d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
                           fill="currentFill"
                         />
                       </svg>
+
                       <span className="sr-only">Loading...</span>
                     </div>
                   )}
+
                   <div>sending...</div>
                 </>
               )}
