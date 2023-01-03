c1c78965a32e49a8c820507346803817e110f5b6
Finley Breese <finley@fmprotocol.com>
2023-01-03 13:26:35 -0500

    test(GenericExpandingSelector): fix storybook story

--- ui2/src/components/GenericExpandingSelector/GenericExpandingSelector.story.tsx
+++ ui2/src/components/GenericExpandingSelector/GenericExpandingSelector.story.tsx
@@ -3,6 +3,8 @@ import {
   useViewModel,
 } from '@component/CrossChainDepositLayout/useViewModel'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { useAnimationControls } from 'framer-motion'
+import { useRef } from 'react'
 
 import { GenericExpandingSelector as Component } from './GenericExpandingSelector'
 
@@ -16,7 +18,35 @@ export const GenericExpandingSelector: ComponentStory<
 > = () => {
   const vm = useViewModel(initialState)
   const { formEditingMode, tokenSearchValue, dispatch } = vm
-  return <Component {...{ formEditingMode, tokenSearchValue, dispatch }} />
+  const chainSelectorButtonControls = useAnimationControls()
+  const tokenSelectorButtonControls = useAnimationControls()
+
+  const chainSelectorRef = useRef<HTMLButtonElement>(null)
+  const tokenSelectorRef = useRef<HTMLButtonElement>(null)
+  const tokenSelectorContainerRef = useRef<HTMLDivElement>(null)
+  const tokenSearchRef = useRef<HTMLInputElement>(null)
+  const chainSelectorContainerRef = useRef<HTMLDivElement>(null)
+  const chainSearchRef = useRef<HTMLInputElement>(null)
+  const chainSelectorResultsContainerRef = useRef<HTMLDivElement>(null)
+
+  return (
+    <Component
+      {...{ formEditingMode, tokenSearchValue, dispatch }}
+      refs={{
+        chainSelector: chainSelectorRef,
+        tokenSelector: tokenSelectorRef,
+        tokenSelectorContainer: tokenSelectorContainerRef,
+        tokenSearch: tokenSearchRef,
+        chainSelectorContainer: chainSelectorContainerRef,
+        chainSearch: chainSearchRef,
+        chainSelectorResultsContainer: chainSelectorResultsContainerRef,
+      }}
+      controls={{
+        chainSelectorButton: chainSelectorButtonControls,
+        tokenSelectorButton: tokenSelectorButtonControls,
+      }}
+    />
+  )
 }
 
 GenericExpandingSelector.args = {}
