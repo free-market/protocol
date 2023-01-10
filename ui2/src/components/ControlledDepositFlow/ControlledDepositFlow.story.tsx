adf6980f781e1e19e958ec376f1af9bd5081f9c1
Finley Breese <finley@fmprotocol.com>
2023-01-10 15:21:38 -0500

    chore(FMP-358): setup Wagmi for ControlledDepositFlow

--- ui2/src/components/ControlledDepositFlow/ControlledDepositFlow.story.tsx
+++ ui2/src/components/ControlledDepositFlow/ControlledDepositFlow.story.tsx
@@ -1,13 +1,34 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { publicProvider } from 'wagmi/providers/public'
+import { configureChains, createClient, WagmiConfig, mainnet } from 'wagmi'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 
 import { ControlledDepositFlow as Component } from './ControlledDepositFlow'
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet],
+  [publicProvider()],
+)
+
+const client = createClient({
+  provider,
+  connectors: [new MetaMaskConnector({ chains })],
+  webSocketProvider,
+})
 
 export default {
   title: 'Example/ControlledDepositFlow',
   component: Component,
+  decorators: [
+    (Story) => (
+      <WagmiConfig client={client}>
+        <Story />
+      </WagmiConfig>
+    ),
+  ],
 } as ComponentMeta<typeof Component>
 
-export const ControlledDepositFlow: ComponentStory<typeof Component> = () => (
-  <Component />
-)
+export const ControlledDepositFlow: ComponentStory<typeof Component> = (
+  props,
+) => <Component {...props} />
 ControlledDepositFlow.args = {}
