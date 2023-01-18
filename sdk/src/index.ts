8d922d59fad76ca2c40f1a0a73fd8b140d868953
Finley Breese <finley@fmprotocol.com>
2023-01-18 10:10:13 -0500

    fix(sdk/src/index.ts): remove unused import

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -5,7 +5,6 @@
 
 export * from './types'
 export * from './builder/WorkflowBuilder'
-export { getEthConfig } from '@fmp/evm'
 export * from './actions'
 export * from './assetInfo'
 export * from './chainInfo'
