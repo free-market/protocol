9cc92c0783f14cdb01a14169d9eb0efec8a9d84f
Finley Breese <finley@fmprotocol.com>
2023-01-18 10:09:56 -0500

    chore: gracefully load .env

--- evm/start-ganache.sh
+++ evm/start-ganache.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -x
 set -e
-source .env
-./node_modules/.bin/ganache --fork  $MAINNET_ETHEREUM --verbose --mnemonic "$GANACHE_MNEMONIC" 
+
+if [ -f .env ]; then
+  source .env
+fi
+
+./node_modules/.bin/ganache --fork  $MAINNET_ETHEREUM --verbose --mnemonic "$GANACHE_MNEMONIC"
