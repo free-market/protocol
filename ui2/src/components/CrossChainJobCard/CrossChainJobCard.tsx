4afbe9a5dbcdd7f0c1be7e5d88e7e4f74f9f8ffa
Finley Breese <finley@fmprotocol.com>
2023-02-23 17:13:27 -0500

    fix(FMP-418): use promise queue for fee prediction
    also fixex FMP-417

--- ui2/src/components/CrossChainJobCard/CrossChainJobCard.tsx
+++ ui2/src/components/CrossChainJobCard/CrossChainJobCard.tsx
@@ -1,3 +1,4 @@
+import { FeePredictionData } from '@component/DepositFlowStateProvider/types'
 import {
   ArrowRightIcon,
   ArrowTopRightOnSquareIcon,
@@ -19,6 +20,11 @@ export const CrossChainJobCard = (props: {
   sourceNetwork?: NetworkName
   sourceTransaction?: { hash: string }
   destinationTransaction?: { hash: string }
+  destinationGasAmount?: string
+  destinationGasSymbol?: string
+  fee?: FeePredictionData
+  sourceGasAmount?: string
+  sourceGasSymbol?: string
 }): JSX.Element => {
   const {
     status = 'completed',
@@ -29,6 +35,22 @@ export const CrossChainJobCard = (props: {
     sourceNetwork = 'ethereum',
     sourceTransaction,
     destinationTransaction,
+    fee = {
+      slippage: '0.5%',
+      destination: {
+        gasPrice: '100',
+      },
+      source: {
+        gasPrice: '100',
+      },
+      protocol: {
+        usd: '0.00',
+      },
+      lowestPossibleAmount: '0',
+      suppliedAmount: '0.00',
+    },
+    destinationGasSymbol = 'ETH',
+    sourceGasSymbol = 'ETH',
   } = props
 
   const [clicked, setClicked] = useState(false)
@@ -499,29 +521,30 @@ export const CrossChainJobCard = (props: {
                   <div className="space-y-1">
                     <div className="flex items-center justify-between text-xs px-4 border border-transparent text-stone-300 font-light">
                       <div>Slippage</div>
-                      <div>0.5%</div>
+                      <div>{fee.slippage}</div>
                     </div>
                     <div className="flex items-center justify-between text-xs px-4 border border-transparent text-stone-300 font-light">
                       <div>Gas on destination</div>
-                      <div>0.00235148 ETH</div>
+                      <div>
+                        {fee.destination.gasPrice} {destinationGasSymbol}
+                      </div>
                     </div>
                     <div className="flex items-center justify-between text-xs px-4 border border-transparent text-stone-400 font-light">
                       <div>Fee</div>
                       <div className="flex items-center gap-1">
-                        $3.00{' '}
-                        <svg
-                          className="w-4 h-4 fill-stone-400"
-                          focusable="false"
-                          viewBox="0 0 24 24"
-                          aria-hidden="true"
-                        >
-                          <path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path>
-                        </svg>
+                        $
+                        {Math.ceil(
+                          (Number(fee.suppliedAmount) -
+                            Number(fee.protocol.usd)) *
+                            100,
+                        ) / 100}
                       </div>
                     </div>
                     <div className="flex items-center justify-between text-xs px-4 border border-transparent text-stone-400 font-light">
                       <div>Gas cost</div>
-                      <div>1.435278 AVAX</div>
+                      <div>
+                        {fee.source.gasPrice} {sourceGasSymbol}
+                      </div>
                     </div>
                   </div>
                 </div>
