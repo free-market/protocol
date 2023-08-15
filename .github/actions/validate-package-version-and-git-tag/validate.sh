729c45f3f3c49fa8ee99303df0ef843c4fb8a591
Marty Saxton <marty@fmprotocol.com>
2023-08-15 08:40:45 -0500

    publish all packages

--- .github/actions/validate-package-version-and-git-tag/validate.sh
+++ .github/actions/validate-package-version-and-git-tag/validate.sh
@@ -3,19 +3,39 @@
 prefix=$1
 gitRef=$2
 
-if [[ $gitRef =~ $prefix(.*) ]]; then
-  refVersion=${BASH_REMATCH[1]}
-  echo "gitRef '$gitRef' is prefixed by '$prefix'"
-  packageVersion=`cat client-sdk/package.json | jq -r '.version'`
-  echo "gitRef version  = $refVersion"
-  echo "package version = $packageVersion"
-  if [ $packageVersion == $refVersion ]; then
-    echo versions match
-    exit 0
-  else
-    echo versions do not match
-    exit 1
+verifyTagMatchesPackageJsonVersion() {
+  if [[ $gitRef =~ $prefix(.*) ]]; then
+    refVersion=${BASH_REMATCH[1]}
+    # echo "gitRef '$gitRef' is prefixed by '$prefix'"
+    packageVersion=`cat $1 | jq -r '.version'`
+    # echo $packageVersion
+    # echo "gitRef version  = $refVersion"
+    # echo "package version = $packageVersion"
+    if [ $packageVersion == $refVersion ]; then
+      echo $1: gitRef $gitRef is prefixed by $prefix and versions match
+      return
+      # exit 0
+    else
+      echo $1 versions do not match
+      exit 1
+    fi
   fi
-fi
-echo "gitRef '$gitRef' is does not prefixed by '$prefix'"
-exit 1
+  echo "$1 gitRef '$gitRef' is does not prefixed by '$prefix'"
+  exit 1
+}
+
+packageJsons=`find . -maxdepth 4 -type f -name package.json | grep -v node_modules`
+for pkgj in $packageJsons; do
+  verifyTagMatchesPackageJsonVersion $pkgj
+done
+
+# a='refs/tags/client-sdk-0.2.0'
+# b='refs/tags/client-sdk-'
+# echo "x gitRef '$gitRef' is prefixed by '$prefix'"
+# if [[ $gitRef =~ $prefix(.*) ]]; then
+#   echo omg
+#   echo ${BASH_REMATCH[0]}
+#   echo ${BASH_REMATCH[1]}
+#   echo ${BASH_REMATCH[2]}
+#   echo --
+# fi
