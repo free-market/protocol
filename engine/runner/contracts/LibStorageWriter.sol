46e4a4ed38469ecc64e5bfcf711bf5ca07e839c5
Marty Saxton <marty@fmprotocol.com>
2024-02-18 12:14:26 -0600

    FM-28 use encodeCall instead of encodeWithSignature

--- engine/runner/contracts/LibStorageWriter.sol
+++ engine/runner/contracts/LibStorageWriter.sol
@@ -1,46 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
+import {EternalStorage} from './EternalStorage.sol';
 
 library StorageWriter {
   // *** Setter Methods ***
   function setUint(address storageAddr, bytes32 key, uint256 value) internal {
-    (bool success, bytes memory returnData) = storageAddr.delegatecall(abi.encodeWithSignature('setUint(bytes32,uint256)', key, value));
+    (bool success, bytes memory returnData) = storageAddr.delegatecall(abi.encodeCall(EternalStorage.setUint, (key, value)));
     require(success, string(returnData));
   }
 
   function setString(address storageAddr, bytes32 key, string memory value) internal {
-    (bool success, bytes memory returnData) = storageAddr.delegatecall(
-      abi.encodeWithSignature('setString(bytes32,string memory)', key, value)
-    );
+    (bool success, bytes memory returnData) = storageAddr.delegatecall(abi.encodeCall(EternalStorage.setString, (key, value)));
     require(success, string(returnData));
   }
 
   function setAddress(address storageAddr, bytes32 key, address value) internal {
-    (bool success, bytes memory returnData) = storageAddr.delegatecall(abi.encodeWithSignature('setAddress(bytes32,address)', key, value));
+    (bool success, bytes memory returnData) = storageAddr.delegatecall(abi.encodeCall(EternalStorage.setAddress, (key, value)));
     require(success, string(returnData));
   }
 
   function setBytes(address storageAddr, bytes32 key, bytes memory value) internal {
-    (bool success, bytes memory returnData) = storageAddr.delegatecall(
-      abi.encodeWithSignature('setBytes(bytes32,bytes memory)', key, value)
-    );
+    (bool success, bytes memory returnData) = storageAddr.delegatecall(abi.encodeCall(EternalStorage.setBytes, (key, value)));
     require(success, string(returnData));
   }
 
   function setBool(address storageAddr, bytes32 key, bool value) internal {
-    (bool success, bytes memory returnData) = storageAddr.delegatecall(abi.encodeWithSignature('setBool(bytes32,bool)', key, value));
+    (bool success, bytes memory returnData) = storageAddr.delegatecall(abi.encodeCall(EternalStorage.setBool, (key, value)));
     require(success, string(returnData));
   }
 
   function setInt(address storageAddr, bytes32 key, int256 value) internal {
-    (bool success, bytes memory returnData) = storageAddr.delegatecall(abi.encodeWithSignature('setInt(bytes32,int256)', key, value));
-    require(success, string(returnData));
-  }
-
-  function setStepAddress(address storageAddr, uint16 stepTypeId, address stepAddress) internal {
-    (bool success, bytes memory returnData) = storageAddr.delegatecall(
-      abi.encodeWithSignature('setStepAddress(uint16,address)', stepTypeId, stepAddress)
-    );
+    (bool success, bytes memory returnData) = storageAddr.delegatecall(abi.encodeCall(EternalStorage.setInt, (key, value)));
     require(success, string(returnData));
   }
 }
