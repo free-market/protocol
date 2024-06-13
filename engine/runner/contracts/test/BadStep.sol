d4d918e9ba47ce2ee3fedb4e9353ccb4e37bc96f
Jonathan Wolff <jwolff@isentropy.com>
2024-06-13 17:22:35 -0400

    rm comment

--- engine/runner/contracts/test/BadStep.sol
+++ engine/runner/contracts/test/BadStep.sol
@@ -22,22 +22,7 @@ contract BadStep is IWorkflowStep {
     isUserProxy != isUserProxy;
     owner = address(++i);
     console.log('Running BadStep');
-    /*
-      AssetAmount[] inputAssetAmounts;
-  // The amounts of each output asset that resulted from the step execution.
-  AssetAmount[] outputAssetAmounts;
-  // The amounts of each output asset that resulted from the step execution.
-  AssetAmount[] outputAssetAmountsToCaller;
-  // The index of the next step in a workflow.
-  // This value allows the step to override the default nextStepIndex
-  // statically defined
-  // -1 means terminate the workflow
-  // -2 means do not override the statically defined nextStepIndex in WorkflowStep
-  int16 nextStepIndex;
-  // the fee to be withheald out of the output assets (in absolute currency units)
-  // -1 means use the default fee
-  int24 fee;
-  */
+
     WorkflowStepResult memory rslt = WorkflowStepResult({
         inputAssetAmounts: inputAssetAmounts,
         outputAssetAmounts: inputAssetAmounts,
